6cec956ccf542597a83b4f374d10c462ccf7faef
Sakura-Q <1067458966@qq.com>
2019-09-15 10:32:35 +0800

    另一个树的子树

--- leetcode_572.cpp
+++ leetcode_572.cpp
@@ -10,6 +10,20 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* s, TreeNode* t) {
-        
+        if(!s)
+			return false;
+		if(sameTree(s,t))
+			return true;
+		return isSubtree(s->left,t)||isSubtree(s->right,t);
     }
+	bool sameTree(TreeNode* s,TreeNode* t){
+		if(!s&&!t)
+			return true;
+		if(!s||!t)
+			return false;
+		if(s->val==t->val)
+			return sameTree(s->left,t->left)&&sameTree(s->right,t->right);
+		else
+			return false;
+	}
 };
