338f9616d610d8b7485a407f6d8a5896ea0712c0
Sakura-Q <1067458966@qq.com>
2019-09-15 10:50:47 +0800

    翻转二叉树

--- leetcode_226.cpp
+++ leetcode_226.cpp
@@ -10,6 +10,15 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        
-    }
+        if(root==NULL)
+			return NULL;
+		TreeNode* tmp=root->left;
+		root->left=root->right;
+		root->right=tmp;
+		if(root->left)
+			invertTree(root->left);
+		if(root->right)
+			invertTree(root->right);
+		return root;
+	}
 };
